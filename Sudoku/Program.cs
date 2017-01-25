e143af663c652afce0617ab50afe27158fc63e2a
Владимир Новопашин < >
2017-01-25 15:12:16 +0300

    Add sudoku

---
+++ Sudoku/Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading;
+
+namespace Sudoku
+{
+    class Program
+    {
+        Sudoku sudoku;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+            Console.ReadKey();
+        }
+
+        public void Start() {
+            PrintFrame();
+            sudoku = new Sudoku(PrintDigit, SaveAnswer);
+            LoadSudoku("sudoku.txt");
+            sudoku.Solve();
+            /*do {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                GenerateRandom(70);            
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.ReadKey();
+            } while (!sudoku.Solve());
+            */
+            
+        }
+
+        void SaveAnswer(){
+            using (StreamWriter file = new StreamWriter("solver.txt", false, Encoding.GetEncoding(1251) )){
+                string symbol = " ";
+                for (int py = 0; py <= Sudoku.max + Sudoku.sqr; py++)
+                {
+                    for (int px = 0; px <= Sudoku.max + Sudoku.sqr; px++)
+                    {
+                        if (px % (Sudoku.sqr + 1) == 0 &&
+                            py % (Sudoku.sqr + 1) == 0)
+                            symbol = "+";
+                        else
+                            if (px % (Sudoku.sqr + 1) == 0)
+                                symbol = "|";
+                            else
+                                if (py % (Sudoku.sqr + 1) == 0)
+                                    symbol = "-";
+                                else
+                                {
+                                    int x = px - 1 - px / (Sudoku.sqr + 1);
+                                    int y = py - 1 - py / (Sudoku.sqr + 1);
+                                    symbol = sudoku.map[x, y].ToString();
+                                }
+                        file.Write(symbol);
+                    }
+                    file.WriteLine();
+                }
+                file.WriteLine("Головоломка решена за {0} шагов.", sudoku.count);
+            }
+        }
+
+        private void GenerateRandom(int count)
+        {
+            Random objRandom = new Random();
+            if (count == 0 ||
+                count> Sudoku.max * Sudoku.max) return;
+            sudoku.ClearMap();
+            for (int c = 0; c < count; c++)
+            {
+                int x=0, y=0, d=0, loop=500;
+                do {
+                    x = objRandom.Next(0, Sudoku.max);
+                    y = objRandom.Next(0, Sudoku.max);
+                    d = objRandom.Next(1, Sudoku.max+1);
+                } while (--loop>0 && !sudoku.PlaceDigit (x,y, d));
+            }
+            for (int x = 0; x < Sudoku.max; x++)
+            for (int y = 0; y < Sudoku.max; y++)
+                sudoku.PlaceDigit(x, y, objRandom.Next(1, Sudoku.max+1));
+        }
+
+        public void LoadSudoku(string filename) {
+            string[] lines = File.ReadAllLines(filename);
+            int y=0; 
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string s = lines[i];
+                if (lines[i].Contains("---")) continue;
+                s = s.Replace("!", "");
+                s = s.Replace('.', '0');
+                for (int x = 0; x < s.Length; x++)                
+                    sudoku.PlaceDigit (x,y, int.Parse ( s.Substring (x,1)));                
+                y++;
+            }
+        }
+        
+        public void PrintFrame() {
+
+            string symbol =" ";
+            for (int px = 0; px <= (Sudoku.sqr+1) * Sudoku.sqr; px++)            
+            for (int py = 0; py <= Sudoku.max +  Sudoku.sqr; py++)
+            {
+                if (px % (Sudoku.sqr + 1) == 0 &&
+                    py % (Sudoku.sqr + 1) == 0)
+                    symbol = "+";
+                else
+                    if (px % (Sudoku.sqr + 1) == 0)
+                        symbol = "|";
+                    else
+                        if (py % (Sudoku.sqr + 1) == 0)
+                            symbol = "-";
+                        else
+                            symbol = " ";
+                Console.SetCursorPosition(px, py);
+                Console.Write(symbol);                
+            }
+            
+        }
+
+        public void PrintDigit(int x, int y, int d){
+            int px = 1 + x + x / Sudoku.sqr;
+            int py = 1 + y + y / Sudoku.sqr;
+            Console.SetCursorPosition(px, py);
+            Console.Write(d==0? " ": d.ToString());
+            Thread.Sleep(1);
+        }
+
+        
+    }
+}
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Sudoku
+{
+    delegate void PrintDigitDelegate(int x, int y, int d);
+    delegate void SaveAnswerDelegate();
+
+    class Sudoku
+    {
+        public const int max = 9; // размер поля
+        public const int sqr = 3; // размер квадратика
+
+        public int[,] map { get; private set; }
+
+        PrintDigitDelegate  PrintDigit;
+        SaveAnswerDelegate SaveAnswer;
+
+        public Sudoku(PrintDigitDelegate printDigit
+            , SaveAnswerDelegate saveAnswer) {
+            map = new int[max, max];
+            PrintDigit = printDigit;
+            SaveAnswer = saveAnswer;
+            ClearMap();
+
+        }
+
+        public void ClearMap() {
+            for (int x = 0; x < max; x++)
+                for (int y = 0; y < max; y++)
+                    ClearDigit(x, y);
+        }
+
+        public bool PlaceDigit(int x, int y, int d){
+            if (x < 0 || x >= max) return false;
+            if (y < 0 || y >= max) return false;
+            if (d <= 0 || d > Sudoku.max) return false;
+
+            if (map[x, y] == d) return true;
+            if (map[x, y] != 0) return false;
+
+            for (int cx = 0; cx < max; cx++)
+                if (map[cx, y] == d) return false;
+
+            for (int cy = 0; cy < max; cy++)
+                if (map[x, cy] == d) return false;
+            //Координата левого верхнего угла текущего квадрата
+            int sx = Sudoku.sqr * (x / Sudoku.sqr);
+            int sy = Sudoku.sqr * (y / Sudoku.sqr);
+
+            for (int cx = sx; cx < sx + sqr; cx++)
+            for (int cy = sy; cy < sy + sqr; cy++)
+                if (map[cx, cy] == d) return false;
+
+            map[x, y] = d;
+            PrintDigit(x, y, d);
+            return true;
+        }
+
+        //Найдено ли решение
+        bool found = false;
+        public int count = 0;
+        public bool Solve()
+        {
+            count = 0;
+            NextDigit(0); //вызываем с начальной клетки
+            return found;
+        }
+
+        void NextDigit(int step) {
+            if (found) return;
+
+            if (step == max * max){
+                found = true;
+                SaveAnswer();
+                SaveAnswerPrivate();
+                return;
+            }
+            count++;
+            int x = step % max;
+            int y = step / max;
+
+            if (map[x, y] > 0) {
+                NextDigit(step + 1);
+                return;
+            }
+
+            for (int d = 1; d <=max; d++)
+                if (PlaceDigit(x, y, d))
+                {
+                    NextDigit(step + 1);
+                    ClearDigit(x, y);
+                }
+        }
+
+        void ClearDigit(int x, int y) {
+            if (x < 0 || x >= max) return;
+            if (y < 0 || y >= max) return;
+            if (map[x, y] == 0) return;
+            map[x, y] = 0;
+            PrintDigit(x, y, 0);
+            
+        }
+
+        void SaveAnswerPrivate(){
+            Console.SetCursorPosition(0, max + sqr + 1);
+            Console.Write("Количество вызовов рекурсивной функции - {0}", count);
+            Console.ReadKey();
+        }
+
+
+    }
+}
